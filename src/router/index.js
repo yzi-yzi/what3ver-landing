bf2458ab153996661810447450a55577dd586756
kaneabel <tttuan0311@gmail.com>
2022-05-30 21:19:17 +0700

    fix: change meta tag content

--- src/router/index.js
+++ src/router/index.js
@@ -38,7 +38,7 @@ const router = new VueRouter({
 router.afterEach((to) => {
   if (to.meta && to.meta.title) {
     document.title =
-      to.meta.title + " | Thương hiệu thời trang nữ";
+      to.meta.title + " - Thời trang nữ";
   }
 });
 
